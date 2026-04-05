ebf1536299c77221397b7c8ad50bd1ace490f7e9
Kedelly <ellyjumah058@gmail.com>
2026-04-06 01:21:56 +0300

    Update Firebase configuration with real credentials

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -11,12 +11,12 @@
  */
 
 const firebaseConfig = {
-  apiKey:            "PASTE_YOUR_API_KEY_HERE",
-  authDomain:        "PASTE_YOUR_AUTH_DOMAIN_HERE",
-  projectId:         "PASTE_YOUR_PROJECT_ID_HERE",
-  storageBucket:     "PASTE_YOUR_STORAGE_BUCKET_HERE",
-  messagingSenderId: "PASTE_YOUR_MESSAGING_SENDER_ID_HERE",
-  appId:             "PASTE_YOUR_APP_ID_HERE"
+  apiKey: "AIzaSyBOEutlV1_c73mmaOqDOjGLvk5CDf_jQD4",
+  authDomain: "namuspaces-main.firebaseapp.com",
+  projectId: "namuspaces-main",
+  storageBucket: "namuspaces-main.firebasestorage.app",
+  messagingSenderId: "133991567956",
+  appId: "1:133991567956:web:0a75d2ff695dcb135113c1"
 };
 
 /* ---- Initialise Firebase ---- */
